ed48b4eb4a4d2a87e596d4df7b3a2fdeb84de055
hamjihun-1 <fkalsj46@gmail.com>
2025-11-21 17:55:47 +0900

    123

--- parktel/js/main.js
+++ parktel/js/main.js
@@ -222,7 +222,23 @@ $(document).ready(function(){
         switchRoomType($(this));
     });
 
-
+/************facilties swiper 시작************/
+    const facilties_swiper = new Swiper('.facilties .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            1024: {    /* 640px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+/************facilties swiper 종료************/
 
 
 
