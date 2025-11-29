656e45ec700550ce41f99daee630d7a230e1775c
hamjihun-1 <fkalsj46@gmail.com>
2025-11-29 20:08:49 +0900

    123

--- museum/js/common.js
+++ museum/js/common.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-
-    
-
+    /* 메뉴오픈 클릭시 header에 open 클래스 추가 */
+    $('header .head_top .menu_open').on('click focusin', function(){
+        $('header').addClass('open')
+    })
+    $('header .head_top .menu_close').on('click', function(){
+        $('header').removeClass('open')
+    })
+    $('.visual').on('focusin', function(){
+        $('header').removeClass('open')
+    })
+    /* 메뉴오픈 클릭시 header에 open 클래스 추가 */
 })//맨끝
