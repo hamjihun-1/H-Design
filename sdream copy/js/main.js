f282d153a74cb6b08b92d1ebaa654a77d08fd6a3
hamjihun-1 <fkalsj46@gmail.com>
2026-02-03 22:38:35 +0900

    123

---
+++ sdream copy/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 2500,
+            disableOnInteraction: true,
+        },
+
+        effect: "fade", /* fade 효과 */
+
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+     
+
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.visual .btn_wrap .btn_next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.visual .btn_wrap .btn_prev',  
+        },
+    });
+    // swiper.autoplay.stop();  /* 일시정지 기능 */
+    // swiper.autoplay.start();  /* 재생 기능 */
+
+})
